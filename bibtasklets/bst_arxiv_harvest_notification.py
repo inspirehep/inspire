549f7a2e09a2d5e7bbb2d07be10ab1a933bb9291
Graham R. Armstrong <graham.richard.armstrong@cern.ch>
2013-07-12 10:33:45 +0200

    BibTasklets: Added bst_arxiv_harvest_notification
    * Adds a new tasklets which notifies admins when arXiv harvesting
      has not ran.

---
+++ bibtasklets/bst_arxiv_harvest_notification.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##
+## This file is part of Invenio.
+## Copyright (C) 2013 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Inspire Arxiv Harvest Notification
+
+An alarm system to notify the appropriate person if the daily
+Arxiv harvest has not ran.
+"""
+from invenio.search_engine import perform_request_search
+from invenio.config import CFG_SITE_ADMIN_EMAIL, CFG_WEBALERT_ALERT_ENGINE_EMAIL
+
+
+def bst_arxiv_harvest_notification(admin_email=CFG_SITE_ADMIN_EMAIL):
+    """
+    Checks if Arxiv harvesting has ran today and emails the
+    admin if it has not. This task should be scheduled to run
+    at the end of the day.
+
+    @param admin_email: The person who should be notified
+    @type admin_email: string
+    """
+    # Search string lists all records harvested today from Arxiv
+    if not perform_request_search(p="find da today and eprint arxiv"):
+        from invenio.mailutils import send_email
+        message = """
+            The Arxiv harvest does not appear to have ran
+            today (No new records have been harvested)."""
+
+        send_email(admin_email, CFG_WEBALERT_ALERT_ENGINE_EMAIL,
+            "Notice: Arxiv harvest has not ran today", message)
