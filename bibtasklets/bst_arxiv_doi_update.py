fc17441e63be5b2dc33fb2c16ec48530ad4f7573
Graham R. Armstrong <graham.richard.armstrong@cern.ch>
2013-10-16 16:34:21 +0200

    BibTasklets: Added Asana integration
    * Adds Asana integration to bst_arxiv_doi_update.py
    * Requires API Key from CFG_ASANA_API_KEY in Invenio config.
    * Default parent task and API key can be modified in the starting parameters.
    * In case of any Asana error, DOI errors will be emailed to the instance
      admin instead.

--- bibtasklets/bst_arxiv_doi_update.py
+++ bibtasklets/bst_arxiv_doi_update.py
@@ -27,10 +27,12 @@ import datetime
 from invenio.bibtaskutils import ChunkedBibUpload, ChunkedBibIndex
 from invenio.docextract_record import get_record, BibRecord
 from invenio.search_engine import perform_request_search
-from invenio.config import CFG_TMPSHAREDDIR
+from invenio.config import (CFG_TMPSHAREDDIR,
+                            CFG_SITE_ADMIN_EMAIL)
 from invenio.bibtask import (write_message,
                              task_update_status,
                              task_update_progress)
+from invenio.mailutils import send_email
 
 # NB: For future reference, elementtree.ElementTree is depreciated after
 # Python 2.4, Inspire instances on higher Python versions should use xml.etree
@@ -40,17 +42,28 @@ try:
 except ImportError:
     from xml.etree import ElementTree as ET
 
+try:
+    from invenio.config import CFG_ASANA_API_KEY
+except ImportError:
+    CFG_ASANA_API_KEY = None
 
 MESSAGES = []
 ERRORS = []
 SCRIPT_NAME = "bst_arxiv_doi_update"
 URI_DEFAULT = "https://vendor.ridge.aps.org/arXiv/latest_pub.xml"
+ASANA_PARENT_TASK_ID = 8115229241606
+
+
+class DOIError(Exception):
+    """ Exception to be raised for incorrect DOIs """
+    pass
 
 # ======================== MAIN =======================
 
 
-def bst_arxiv_doi_update(input_uri=None,
-                         log_dir=CFG_TMPSHAREDDIR, logging=True):
+def bst_arxiv_doi_update(input_uri=None, log_dir=CFG_TMPSHAREDDIR, logging=True,
+                         asana_key=CFG_ASANA_API_KEY,
+                         asana_parent_id=ASANA_PARENT_TASK_ID):
     """
     bst_arxiv_doi_update
     Updates DOIs on documents harvested from ArXiv.
@@ -61,8 +74,11 @@ def bst_arxiv_doi_update(input_uri=None,
             NOTE: Test data can be taken from http://arxiv.org/schemas/doi_feed_test.xml
         * log_dir - Directory to store log files in
         * logging - True or False, default True
+        * asana_key - The Asana API, by default uses the value of CFG_ASANA_API_KEY
+            NOTE: Passing the value of None for this parameter will skip writing
+            to Asana and instead email the instance admin
+        * asana_parent_id - The task in Asana to log subtasks to
     """
-
     if input_uri is None:
         _print("Notice: No URI specified, defaulting to " + URI_DEFAULT)
         input_uri = URI_DEFAULT
@@ -84,9 +100,22 @@ def bst_arxiv_doi_update(input_uri=None,
         return False
     root = tree.getroot()
 
+    try:
+        date_el = root.find('date')
+        date_str = '%s-%s-%s' % (date_el.get('year'), date_el.get('month'),
+                                 date_el.get('day'))
+        _print("Processing DOIs last updated on date %s" % date_str)
+    except AttributeError:
+        _print("Warning: Couldn't get last published date of Arxiv DOI feed.")
+
     doi_count = 0
     new_count = 0
-    missing_count = 0
+
+    # Stores any DOIs with have issues with in structure:
+    # Missing: (doi, arxiv preprint_id, published date)
+    # Ambiguous: (doi, arxiv preprint_id, rec_ids)
+    # Incorrect: (rec_id, old-doi, new-doi)
+    problem_dois = {'missing': [], 'ambiguous': [], 'incorrect': []}
 
     task_update_progress("Processing records...")
     # NB: Element.getiterator() is deprecated since version 2.7: Use
@@ -100,31 +129,44 @@ def bst_arxiv_doi_update(input_uri=None,
         rec_id = get_record_by_arxiv_id(arxiv)
         if len(rec_id) == 1:
             rec_id = rec_id[0]
-            record_xml = append_to_record(rec_id, doi, published_date)
+            try:
+                record_xml = append_to_record(rec_id, doi, published_date)
+            except DOIError as ex:
+                problem_dois['incorrect'].append((rec_id, ex.message, doi))
+                continue
             if record_xml:
                 new_count += 1
                 _print("* Now we will run the bibupload and bibindex for " +
-                       str(rec_id) + " record", 5)
-                _print("** We will upload the following xml code " +
+                       "%s record" % rec_id, 5)
+                _print("** We will upload the following xml code %s" %
                        repr(record_xml), 9)
                 bibupload.add(record_xml)
                 bibindex.add(rec_id)
         elif len(rec_id) > 1:
             _print('ERROR: %d records found with matching arXiv ID %s' %
                    (len(rec_id), arxiv))
+            problem_dois['ambiguous'].append((doi, arxiv, repr(rec_id)))
         else:
-            missing_count += 1
-            _print('No record found matching arxiv ID: ' + arxiv, 9)
+            _print('No record found matching arxiv ID: %s' % arxiv, 9)
+            problem_dois['missing'].append((doi, arxiv, published_date))
 
-    _print("======================== FINAL SCORE ========================", 1)
-    _print("DOIs found and processed: " + str(doi_count), 1)
-    _print("Arxiv IDs without corresponding records: " + str(missing_count), 1)
-    _print("Records requiring appends: " + str(new_count), 1)
+    _print("========================| FINAL SCORE |========================", 1)
+    _print("DOIs found and processed: %d" % doi_count, 1)
+    _print("Arxiv IDs without corresponding records: %d"
+           % len(problem_dois['missing']), 1)
+    _print("Arxiv IDs corresponding to multiple records (duplicates): %d"
+           % len(problem_dois['ambiguous']), 1)
+    _print("Inspire records with an incorrect DOI: %d"
+           % len(problem_dois['incorrect']), 1)
+    _print("Records without DOIs requiring appends: %d" % new_count, 1)
 
     if logging:
         task_update_progress("Logging...")
         write_list_to_file(log_dir, 'errors', ERRORS)
         write_list_to_file(log_dir, 'messages', MESSAGES)
+
+    notify_on_errors(problem_dois, log_dir, doi_count, new_count,
+                     asana_key, asana_parent_id)
 
     task_update_progress(SCRIPT_NAME + " finished. %s DOIs processed, %s to add"
                          % (str(doi_count), str(new_count)))
@@ -302,21 +344,27 @@ def record_has_doi(record, rec_id, doi):
     fields = record.find_fields('0247_')
     for field in fields:
         try:
-            if field.get_subfield_values('2')[0] == 'DOI':
+            if 'DOI' in field.get_subfield_values('2'):
                 _print('Record %s already has a doi' % str(rec_id), 6)
-                if field.get_subfield_values('a')[0] == doi:
+                sub_a = field.get_subfield_values('a')
+                if len(sub_a) != 1:
+                    msg = ('Warning: Multiple DOIs (024 7_ $a) found for ' +
+                           'record %d' % rec_id)
+                    ERRORS.append(msg)
+                    _print(msg)
+
+                if doi in sub_a:
                     _print('DOI for %s matches current DOI' % str(rec_id), 7)
                     return True
                 else:
-                    msg = ('ERROR: DOI of ' + str(rec_id) + ' (' +
-                           field.get_subfield_values('a')[0] + ') record is '
-                           + 'different than the new doi (' + doi + ')!')
+                    msg = ('ERROR: DOI of record #%d (%s)' % (rec_id, sub_a[0])
+                           + 'is different than the new doi (%s)!' % doi)
                     ERRORS.append(msg)
                     _print(msg, 2)
-                    return False
+                    raise DOIError(repr(sub_a))
         except IndexError:
             pass
-    _print('No current DOI found for record #%s' % rec_id, 6)
+    _print('No current DOI found for record #%d' % rec_id, 6)
     return False
 
 
@@ -332,3 +380,161 @@ def add_dots(string):
     if string[-1:] != '.':
         out += '.'
     return out
+
+
+def notify_on_errors(dois, log_dir, count_total, count_new, asana_key,
+                     asana_parent_task_id):
+    """ If the dictionary dois is empty, this function does nothing.
+    Else it will attempt to write information about DOI errors to either
+    Asana or, failing that, emailing the admin.
+    Parameters:
+     * dois - dictionary: 3-part dictionary, with a list of DOIs with errors.
+     * log_dor - string: The local directory where logs are stored.
+     * count_total - int: How many DOIs this script has updated.
+     * count_new - int: How many DOIs have been inserted into Inspire.
+     * asana_key - string: The API Key of the user account being used.
+     * asana_parent_task_id -  int: ID of the parent task in Asana for this
+                                    information to be appended to. """
+    if (len(dois['missing']) == 0 and len(dois['ambiguous']) == 0 and
+            len(dois['incorrect']) == 0):
+        return
+
+    mail = False
+    if asana_key:
+        try:
+            from asana.asana import AsanaAPI, AsanaException
+            try:
+                asana_instance = AsanaAPI(asana_key)
+                send_asana_tasks(dois, log_dir, count_total, count_new,
+                                 asana_instance, asana_parent_task_id)
+            except AsanaException as ex:
+                _print("Error occured during Asana update, sending email " +
+                       "instead.")
+                _print(ex.message)
+                mail = True
+        except ImportError as ex:
+            mail = True
+            _print("Warning: Could not import Asana API, sending email" +
+                   " instead.", 1)
+    else:
+        mail = True
+
+    if mail:
+        send_notification_email(dois, log_dir, count_total, count_new)
+
+
+def send_notification_email(dois, log_dir, count_total, count_new):
+    """ If any issues occured during the DOI update, this function
+    notifies the appropriate authorities with details of the error
+    Parameters:
+     * dois - a dictionary with three categories of problem DOIs
+     * log_dir - the directory where the logs are stored """
+    msg_html = """<p>Message from BibTasklet %s:</p>
+
+<p>Problems have occurred while updating the DOIs of records. In total %d DOIs were
+processed, of which %d were processed succesfully.</p>
+
+<p>Further details on these errors can be found in the log files, they should be
+available here: '%s'</p>
+""" % (SCRIPT_NAME, count_total, count_new, log_dir)
+
+    if len(dois['missing']) > 0:
+        msg_html += """
+<h3>No records could be found to match the following arXiv tags:</h3>
+<table>
+    <tr>
+        <td><p><strong>DOI</strong></p></td>
+        <td><p><strong>ArXiv PrePrint ID</strong></p></td>
+        <td><p><strong>Date of Publishing</strong></p></td>
+    </tr>
+"""
+        for doi, arxiv, date in dois['missing']:
+            msg_html += """<tr>
+                        <td><p>%s</p></td>
+                        <td><p>%s</p></td>
+                        <td><p>%s</p></td>
+                      </tr>""" % (doi, arxiv, date)
+        msg_html += "</table>"
+
+    if len(dois['ambiguous']) > 0:
+        msg_html += """
+<h3>Multiple records were found for the following arXiv tags:</h3>
+<table>
+    <tr>
+        <td><p><strong>DOI</strong></p></td>
+        <td><p><strong>ArXiv PrePrint ID</strong></p></td>
+        <td><p><strong>RecIDs found</strong></p></td>
+    </tr>
+"""
+        for doi, arxiv, rec_ids in dois['ambiguous']:
+            msg_html += """<tr>
+                        <td><p>%s</p></td>
+                        <td><p>%s</p></td>
+                        <td><p>%s</p></td>
+                      </tr>""" % (doi, arxiv, rec_ids)
+        msg_html += "</table>"
+
+    if len(dois['incorrect']) > 0:
+        msg_html += """
+<h3>Records found by arXiv tags, but had a different DOI:</h3>
+<table>
+    <tr>
+        <td><p><strong>Record ID</strong></p></td>
+        <td><p><strong>Current DOI</strong></p></td>
+        <td><p><strong>New DOI</strong></p></td>
+    </tr>
+"""
+
+        for r_id, old_doi, new_doi in dois['incorrect']:
+            msg_html += """<tr>
+                        <td><p>%s</p></td>
+                        <td><p>%s</p></td>
+                        <td><p>%s</p></td>
+                      </tr>""" % (r_id, old_doi, new_doi)
+        msg_html += "</table>"
+
+    send_email(CFG_SITE_ADMIN_EMAIL, CFG_SITE_ADMIN_EMAIL,
+               "Errors during ArXiv DOI Update", html_content=msg_html)
+
+
+def send_asana_tasks(dois, log_dir, count_total, count_new, asana, parent):
+    """ If any issues occured during the DOI update, this function
+    logs the faulty DOIs to Asana
+    Parameters:
+     * dois - a dictionary with three categories of problem DOIs
+     * log_dir - the directory where the logs are stored """
+
+    date_str = datetime.datetime.now().strftime("%Y-%m-%d at %H:%M")
+    msg = """Message from BibTasklet %s:
+
+Problems have occurred while updating the DOIs of records. In total %d DOIs were
+processed, of which %d were processed succesfully.
+
+Further details on these errors can be found in the log files, they should be
+available in the following directory: '%s'
+""" % (SCRIPT_NAME, count_total, count_new, log_dir)
+
+    # Create the subtasks
+    parent_task = asana.create_subtask(parent,
+                                       "DOI Update on %s" % date_str,
+                                       notes=msg)
+    par_id = parent_task['id']
+
+    categories = [
+        ('missing', 'Missing Records',
+         "No records could be found to match the following arXiv tags.",
+         "DOI, ArXiv PrePrint ID, Date of Publishing"),
+        ('ambiguous', 'Ambigous Records (duplicates)',
+         "Multiple records were found for the following arXiv tags.",
+         "DOI, ArXiv PrePrint ID, Inspire Record IDs"),
+        ('incorrect', 'Incorrect DOIs',
+         "Records found by arXiv tags, but had a different DOI.",
+         "Record ID, DOI from Inspire, DOI from APS")]
+
+    for dic, title, desc, structure in categories:
+        if len(dois[dic]) > 0:
+            msg = "%s\n\nLine Structure: %s" % (desc, structure)
+            _parent = asana.create_subtask(par_id, title, notes=msg)
+            for prt1, prt2, prt3 in dois[dic]:
+                asana.create_subtask(_parent['id'],
+                                     "%s | %s | %s" % (prt1, prt2, prt3))
