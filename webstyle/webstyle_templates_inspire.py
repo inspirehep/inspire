aa73c438d932da3386741ae448c7b5d111d33bd5
Javier Martin Montull <javier.martin.montull@cern.ch>
2013-10-24 16:49:21 +0200

    Yet another banner

--- webstyle/webstyle_templates_inspire.py
+++ webstyle/webstyle_templates_inspire.py
@@ -422,6 +422,7 @@ $(function () {
    <div class="feedbackboxbody">
  Welcome to <a href="http://www.projecthepinspire.net">INSPIRE</a>, the High Energy Physics information system.
  Please direct questions, comments or concerns to <a href="mailto:feedback@inspirehep.net">feedback@inspirehep.net</a>.
+ <p style="color:red; margin: 2px 0px; font-size: 110%%; font-weight: bold;">We are working on consolidating the service. Some features might experience instabilities. Please, be patient.</p>
    </div>
   </td>
  </tr>
