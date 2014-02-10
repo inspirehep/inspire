8863a8ac2f7bfdc2e0bd2dacd80835f6073634b2
Javier Martin Montull <javier.martin.montull@cern.ch>
2014-02-10 14:30:28 +0100

    BibCheck: modify ticket content in doi plugin

--- bibcheck/plugins/doi.py
+++ bibcheck/plugins/doi.py
@@ -70,8 +70,8 @@ def check_records(records, doi_field="0247_a", extra_subfields=(("2", "DOI"), ("
                         doi,
                         record_id,
                         dup_doi_recid,
-                        "%s/record/%s/edit" % (CFG_SITE_URL, record_id),
-                        "%s/record/%s/edit" % (CFG_SITE_URL, dup_doi_recid),
+                        "%s/record/%s" % (CFG_SITE_URL, record_id),
+                        "%s/record/%s" % (CFG_SITE_URL, dup_doi_recid),
                         "%s/record/merge/?#recid1=%s&recid2=%s" % (CFG_SITE_URL, dup_doi_recid, record_id)
                     )
                     if isinstance(msg, unicode):
