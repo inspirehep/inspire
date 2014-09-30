9db5ebe38a65920830429dae8f9e0462cc9143f8
Jan Aage Lavik <jan.age.lavik@cern.ch>
2014-09-30 13:04:08 +0200

    BibTasklets: webcoll tasklet improvements
    * Improves the the webcoll robotupload tasklet on several levels:
    
    * Adds connection to redis to keep any recids pending if the
      request should fail.
    
    * Fixes an issue with passing arguments to the tasklet.
    
    * Now warns if the config variable CFG_WEBCOLL_POST_REQUEST_URL is not
      set.
    
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- bibtasklets/bst_webcoll_postprocess.py
+++ bibtasklets/bst_webcoll_postprocess.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of INSPIRE.
@@ -18,22 +17,55 @@
 ## You should have received a copy of the GNU General Public License
 ## along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Interact with Labs site to continue submissions when webcoll has run."""
+
 import requests
+
 from invenio.bibtask import (write_message,
                              task_update_status)
-from invenio.config import CFG_WEBCOLL_POST_REQUEST_URL
+from invenio.redisutils import get_redis
+
+try:
+    from invenio.config import CFG_WEBCOLL_POST_REQUEST_URL
+except ImportError:
+    CFG_WEBCOLL_POST_REQUEST_URL = None
 
 
 def bst_webcoll_postprocess(recids=[]):
-    session = requests.Session()
-    addapter = requests.adapters.HTTPAdapter(max_retries=3)
-    session.mount(CFG_WEBCOLL_POST_REQUEST_URL, addapter)
-    response = session.post(CFG_WEBCOLL_POST_REQUEST_URL,
-                            data={'recids': recids})
-    if response.ok:
-        write_message("Post request sent successfully,"
-                      " closing connection!")
+    """Parse recids to POST to remote server to alert that records are visible."""
+    if isinstance(recids, str):
+        recids = recids.split(",")
+    cache = get_redis()
+    cached_ids = cache.get("webcoll_pending_recids") or []
+    recids += cached_ids
+
+    if not CFG_WEBCOLL_POST_REQUEST_URL:
+        write_message("CFG_WEBCOLL_POST_REQUEST_URL is not set.")
+        task_update_status('ERROR')
+        return 1
+
+    if recids:
+        write_message("Going to POST callback to {0}: {1} (total: {2})".format(
+            CFG_WEBCOLL_POST_REQUEST_URL,
+            recids[:10],
+            len(recids))
+        )
+        session = requests.Session()
+        addapter = requests.adapters.HTTPAdapter(max_retries=3)
+        session.mount(CFG_WEBCOLL_POST_REQUEST_URL, addapter)
+        response = session.post(CFG_WEBCOLL_POST_REQUEST_URL,
+                                data={'recids': recids})
+        if response.ok:
+            write_message("Post request sent successfully")
+            cache.set("webcoll_pending_recids", [])
+        else:
+            write_message("Post request failed!")
+            write_message(response.text)
+            task_update_status('ERROR')
+            cache.set("webcoll_pending_recids", recids)
         session.close()
     else:
-        write_message("Post request failed!")
-        task_update_status('ERROR')
+        write_message("No recids to POST callback for to {0}.".format(
+            CFG_WEBCOLL_POST_REQUEST_URL,
+        ))
