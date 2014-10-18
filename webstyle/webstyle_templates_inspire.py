e448400e895b312e4ebf6e1c028c136f67bf7c10
Javier Martin Montull <javier.martin.montull@cern.ch>
2014-10-18 11:59:09 +0200

    Revert "banner: put a banner after power cut"
    This reverts commit 313cf64e67d9b7d59d5f4d4043e95492a2a94fd6.

--- webstyle/webstyle_templates_inspire.py
+++ webstyle/webstyle_templates_inspire.py
@@ -422,9 +422,7 @@ $(function () {
    <div class="feedbackboxbody">
  Welcome to <a href="http://www.projecthepinspire.net">INSPIRE</a>, the High Energy Physics information system.
  Please direct questions, comments or concerns to <a href="mailto:feedback@inspirehep.net">feedback@inspirehep.net</a>.
- <p style="color:red; margin: 2px 0px; font-weight: bold;">Due to the major power cut in our
-Computer Center the INSPIRE service is currently experiencing instability while we are working towards restoring normal conditions</p>
- </div>
+   </div>
   </td>
  </tr>
 </table>
