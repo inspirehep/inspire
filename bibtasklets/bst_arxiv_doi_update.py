c2a7b4ed67bf7c2f5babfe2c32337ae5ebddf917
Jan Aage Lavik <jan.age.lavik@cern.ch>
2015-03-31 10:16:03 +0200

    BibTasklets: no silent update from arXiv<->DOI matcher
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- bibtasklets/bst_arxiv_doi_update.py
+++ bibtasklets/bst_arxiv_doi_update.py
@@ -1,22 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-##
-## This file is part of INSPIRE.
-## Copyright (C) 2013, 2014 CERN.
-##
-## INSPIRE is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public License as
-## published by the Free Software Foundation; either version 2 of the
-## License, or (at your option) any later version.
-##
-## INSPIRE is distributed in the hope that it will be useful, but
-## WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
-## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#
+# This file is part of INSPIRE.
+# Copyright (C) 2013, 2014, 2015 CERN.
+#
+# INSPIRE is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# INSPIRE is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """
 Inspire arXiv DOI update script
@@ -108,7 +108,7 @@ def bst_arxiv_doi_update(input_uri=None,
     task_update_progress("Resolving URI: %s" % (input_uri,))
 
     # Testing builds characters
-    bibupload = ChunkedBibUpload(mode='a', user=SCRIPT_NAME, notimechange=True)
+    bibupload = ChunkedBibUpload(mode='a', user=SCRIPT_NAME, notimechange=False)
 
     # open url and parse xml
     try:
