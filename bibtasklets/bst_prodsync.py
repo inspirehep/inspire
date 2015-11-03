64947837be85eb0cba64b8d91287242b24e68ae8
Samuele Kaplun <samuele.kaplun@cern.ch>
2015-11-03 16:14:22 +0100

    bibtasklet: new bst_prodsync.py
    Signed-off-by: Samuele Kaplun <samuele.kaplun@cern.ch>

---
+++ bibtasklets/bst_prodsync.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##
+## This file is part of INSPIRE.
+## Copyright (C) 2015 CERN.
+##
+## INSPIRE is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## INSPIRE is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with INSPIRE; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Continuously synchronizes to Labs via REDIS"""
+
+import datetime
+import time
+
+import os
+
+from invenio.config import CFG_TMPSHAREDDIR
+
+try:
+    from invenio.config import CFG_REDIS_HOST_LABS
+except ImportError:
+    CFG_REDIS_HOST_LABS = None
+
+from invenio.dbquery import run_sql
+
+from invenio.dateutils import get_time_estimator
+
+from invenio.bibformat_engine import format_record
+
+from invenio.bibtask import task_update_progress, write_message
+
+from invenio.intbitset import intbitset
+
+import redis
+
+
+def bst_prodsync():
+    if CFG_REDIS_HOST_LABS:
+        r = redis.StrictRedis.from_url(CFG_REDIS_HOST_LABS)
+    else:
+        write_message("Redis disabled, writing output into %s" % os.path.join(CFG_TMPSHAREDDIR, "prodsync.xml"))
+        r = open(os.path.join(CFG_TMPSHAREDDIR, "prodsync.xml"), "a")
+    future_lastrun = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    lastrun_path = os.path.join(CFG_TMPSHAREDDIR, 'prodsync_lastrun.txt')
+    try:
+        last_run = open(lastrun_path).read().strip()
+        modified_records = intbitset(run_sql("SELECT id FROM bibrec WHERE modification_date>=%s", (last_run, )))
+        for citee, citer in run_sql("SELECT citee, citer FROM rnkCITATIONDICT WHERE last_updated>=%s", (last_run, )):
+            modified_records.add(citee)
+            modified_records.add(citer)
+        modified_records |= intbitset(run_sql("SELECT bibrec FROM aidPERSONIDPAPERS WHERE last_updated>=%s", (last_run, )))
+    except IOError:
+        # Default to the epoch
+        modified_records = intbitset(run_sql("SELECT id FROM bibrec"))
+    tot = len(modified_records)
+    time_estimator = get_time_estimator(tot)
+    for i, recid in enumerate(modified_records):
+        if CFG_REDIS_HOST_LABS:
+            r.rpush('records', [format_record(recid, 'xme')])
+            # Client should simply use http://redis.io/commands/blpop
+        else:
+            # NOTE: just for debugging purposes
+            print >> r, format_record(recid, 'xme')
+        task_update_progress("%s (%s%%) -> %s" % (recid, (i + 1) * 100 / tot, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time_estimator()[1]))))
+    write_message("Pushed %s records" % tot)
+    open(lastrun_path, "w").write(future_lastrun)
