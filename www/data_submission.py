inspirehep/inspire

commit 1361b3332a0dd57eacc30be6c5a48c15366dec34

replace curators with an e-group list

www/data_submission.py
@@ -74,17 +74,11 @@ def submit_email_ticket(title, paper, authors, description, dataset_file,
     from email import Encoders
     from invenio.config import CFG_MISCUTIL_SMTP_HOST, CFG_MISCUTIL_SMTP_PORT
 
-    curators = (
-        "laura.rueda@cern.ch",
-        "patricia.herterich@cern.ch",
-        "sunje.dallmeier-tiessen@cern.ch"
-    )
-
     msg = MIMEMultipart()
     msg['Subject'] = "New dataset submission from {0}".format(submitter_name).encode("utf-8")
     msg['From'] = submitter_email
-    msg['To'] = ', '.join(curators)
-
+    msg['To'] = "inspire-data-submission@cern.ch"
+    
     text = """
     Title: %s
     Original paper: %s

commit 64acd2c582e968e97f0aaac982e092ed1236f47e

data: fix previous commit
* Fixes bug introduced with previous commit
  1361b3332a0dd57eacc30be6c5a48c15366dec34.

Signed-off-by: Samuele Kaplun <samuele.kaplun@cern.ch>

www/data_submission.py
@@ -74,11 +74,13 @@ def submit_email_ticket(title, paper, authors, description, dataset_file,
     from email import Encoders
     from invenio.config import CFG_MISCUTIL_SMTP_HOST, CFG_MISCUTIL_SMTP_PORT
 
+    curators = "inspire-data-submission@cern.ch"
+
     msg = MIMEMultipart()
     msg['Subject'] = "New dataset submission from {0}".format(submitter_name).encode("utf-8")
     msg['From'] = submitter_email
-    msg['To'] = "inspire-data-submission@cern.ch"
-    
+    msg['To'] = curators
+
     text = """
     Title: %s
     Original paper: %s