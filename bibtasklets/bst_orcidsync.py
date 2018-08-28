6458eafd2b138740b921baabccddc7387ddec09b
turtle321 <turtle321@foo.bar>
2018-08-28 16:37:08 +0200

    OrcidSync: get users' names and emails from HepNames (if not uid)

--- bibtasklets/bst_orcidsync.py
+++ bibtasklets/bst_orcidsync.py
@@ -33,6 +33,7 @@ SELECT personid, count(*) as cnt FROM aidPERSONIDDATA WHERE tag='uid' OR tag='ex
 '3', '1'
 """
 
+import re
 import redis
 import json
 
@@ -44,24 +45,81 @@ except ImportError:
 from invenio.dbquery import run_sql
 from invenio.webuser import collect_user_info
 from invenio.bibtask import write_message
+from invenio.bibauthorid_webapi import get_hepnames
 
 CFG_REDIS_ORCID_SYNC_KEY = 'legacy_orcid_tokens'
 CFG_REDIS_ORCID_SYNC_KEY_COUNT = 'legacy_orcid_tokens_count'
 
 
 def orcid_data_to_sync():
-    """Returns an iterator of data to sync."""
+    """
+    Collect all available OAuth tokens for ORCID and their related data:
+    (orcid, token, email, name)
+    Orcid and token are mandatory data.
+    Email and name can be empty strings.
+    In order to collect the email and the name, the strategy is:
+    - if a uid is found, then try to get them from the user table.
+    - if the email or the name are empty, then try to get them from HepNames.
+
+    Note regarding account types.
+    There are 2 different types of account involved.
+    1. The Inspire user account that requires an email and name and assigns
+    a uid (in particular when users log in via arXiv). The BAI system personid
+    is separate and independent of the Inspire user accounts uid, but if a
+    connection can be made to a user account the uid is associated with the
+    personid in the aidPERSONIDDATA table.
+    2. HepNames which has personal data like name and email and is associated
+    with the BAI via MARC: 000996436 035__ $$9BAI$$aSimona.Murgia.1
+    There are some convenience methods to get info from HepNames:
+    from invenio.bibauthorid_webapi import get_hepnames
+    """
     rows = run_sql("SELECT personid, data, token FROM aidPERSONIDDATA NATURAL JOIN aidTOKEN WHERE tag='extid:ORCID';")
     for personid, orcid, token in rows:
+        orcid = orcid.decode('utf8')
+        token = token.decode('utf8')
+        email = u''
+        name = u''
+
         uid = run_sql("SELECT data FROM aidPERSONIDDATA WHERE personid=%s AND tag='uid'", (personid, ))
+
+        # If uid, then collect name and email from the user table.
         if uid:
             uid = int(uid[0][0])
             user_info = collect_user_info(uid)
+            email = _validate_email(user_info['email'].decode('utf8'))
             if user_info.get('external_firstname') and user_info.get('external_familyname'):
                 name = u'%s %s' % (user_info['external_firstname'].decode('utf8'), user_info['external_familyname'].decode('utf8'))
             else:
                 name = user_info['nickname'].decode('utf8')
-            yield orcid.decode('utf8'), token.decode('utf8'), user_info['email'].decode('utf8'), name
+
+        # If name or email are empty, then try to collect them from HepNames.
+        hepnames = {}
+        if not email or not name:
+            hepnames = get_hepnames(personid)
+
+        # hepnames['have_hep'] is true if an exact match was found.
+        # Otherwise it is a dictionary of possible choices (useless in this
+        # context).
+        have_hep = hepnames.get('have_hep')
+
+        if have_hep and not email:
+            emails = hepnames.get('record', {}).get('emails')  # 'emails' can be an empty list.
+            email = _validate_email(emails[0].decode('utf8')) if emails else u''
+
+        if have_hep and not name:
+            name = hepnames.get('record', {}).get('display_name', '').decode('utf8')
+
+        yield orcid, token, email, name
+
+
+def _validate_email(email):
+    """
+    Basic validation for email addresses.
+    """
+    email_regex = re.compile(r'^[^@\s]+@[^@\s]+\.[^@\s]+$')
+    if not email_regex.match(email):
+        return u''
+    return email
 
 
 def count_orcid_data_to_sync():
