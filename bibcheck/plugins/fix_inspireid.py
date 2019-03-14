c9d7a17cf0b1f7b40de33b7c409c3b6a1cc3e215
Melissa Clegg <cleggm1@fnal.gov>
2019-03-14 20:40:35 +0100

    BibCheck: updated fix_inspireid plugin
    Signed-off-by: Melissa Clegg <cleggm1@fnal.gov>

--- bibcheck/plugins/fix_inspireid.py
+++ bibcheck/plugins/fix_inspireid.py
@@ -96,7 +96,12 @@ CHANGES = [
     (('u', 'IRFU, Saclay'), ('i', 'INSPIRE-00220215', 'INSPIRE-00657399')),
     (('u', 'DAPNIA, Saclay'), ('i', 'INSPIRE-00220215', 'INSPIRE-00657399')),
     (('a', 'Tudorache, Alexandra'), ('j', 'ORCID:0000-0001-5384-3843', 'ORCID:0000-0001-6307-1437')),
-    (('u', 'Jyvaskyla U.'), ('i', 'INSPIRE-00250377', 'INSPIRE-00493721'))
+    (('u', 'Jyvaskyla U.'), ('i', 'INSPIRE-00250377', 'INSPIRE-00493721')),
+    (('a', 'Zhang, Huaqiao'), ('i', 'INSPIRE-00227070', 'INSPIRE-00227084')),
+    (('i', 'INSPIRE-00317229'), ('a', 'Ahmad, Muhammad', 'Ahmad, Muhammad, 1')),
+    (('i', 'INSPIRE-00365887'), ('a', 'Ahmad, Muhammad', 'Ahmad, Muhammad, 2')),
+    (('i', 'INSPIRE-00536572'), ('a', 'Wang, Jian', 'Wang, Jian, 1')),
+    (('i', 'INSPIRE-00000419'), ('a', 'Wang, Jian', 'Wang, Jian, 2'))
     ]
 
 
